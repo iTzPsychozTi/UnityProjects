028ad92906b8bec9bfd1e3af30b0848978f38156
iTzPsychozTi <krichardson3@mail.bradley.edu>
2022-11-27 22:45:19 -0600

    Prototype/Challenge 5 Submission

---
+++ Prototype5/Assets/Scripts/GameManager.cs
@@ -0,0 +1,84 @@
+﻿/* Kyree Richardson
+ * Prototype 5
+ * (Manages the functionality of the game)
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+
+    private float spawnRate = 1.0f;
+
+    public TextMeshProUGUI scoreText;
+
+    private int score;
+
+    public TextMeshProUGUI gameOverText;
+
+    public bool isGameActive;
+
+    public Button restartButton;
+
+    public GameObject titleScreen;
+
+    public void StartGame(int difficulty)
+    {
+        spawnRate /= difficulty;
+        isGameActive = true;
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        updateScore(0);
+        titleScreen.gameObject.SetActive(false);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void gameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    public void updateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while(isGameActive)
+        {
+            //wait 1 second
+            yield return new WaitForSeconds(spawnRate);
+
+            //pick a random index between 0 and the number of prefabs
+            int index = Random.Range(0, targets.Count);
+
+            //spawn the prefab at the randomly selected index
+            Instantiate(targets[index]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
